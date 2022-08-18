65184c01927f4ff3e118c11f6aaebe3517b278ef
Igor Rezende <igorbrasileiro15@hotmail.com>
2022-08-18 14:48:15 -0300

    deploy

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
-
+require('dotenv').config()
+const Mongo_url = process.env.MONGO_URL
 mongoose.Promise = global.Promise 
 
-mongoose.connect('mongodb://localhost/movetec',{
+mongoose.connect(Mongo_url,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>console.log('conectado ao banco de dados')).catch((e)=>console.log('erro ao conectar banco de dados'))
